32c1587888fadd3decc42755acf678529dcfabad
sayanmondal2098 <sayanmondal2098@gmail.com>
2022-11-10 00:54:30 +0530

    add build

---
+++ build/assets/js/50099d1f.8e91be73.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkknowledgebase=self.webpackChunkknowledgebase||[]).push([[2149],{3769:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
